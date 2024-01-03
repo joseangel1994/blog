5ca69a0c009bed6c637a9f5698c828224be3d9f2
Jose Angel Exposito Arenas <joseangel.expositoarenas@novatec-gmbh.de>
2024-01-03 08:53:48 +0100

    Add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   // used to generate images
@@ -15,7 +16,15 @@ export default defineConfig({
   //   ? `https://${process.env.VERCEL_URL}/`
   //   : 'https://localhost:3000/',
   trailingSlash: 'ignore',
-  integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  integrations: [
+    sitemap(), 
+    UnoCSS({ injectReset: true }),
+    partytown({
+      config: {
+        forward: ['dataLayer.push']
+      }
+    })
+  ],
   experimental: {
     devOverlay: true,
   },
